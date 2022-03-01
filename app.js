c6d3ef9e1af75f1ea4b19ebfd399881fe8e7f592
Karl Reithmeier <kalle_reithmeier@hotmail.com>
2022-03-01 18:51:44 +0100

    still a bit to go

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //app.js
 const cors = require("cors");
 const express = require("express");
+const bodyParser = require('body-parser')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,9 +9,11 @@ const rug = require("random-username-generator");
 
 let users = {};
 
+app.use(bodyParser.json());
 app.use("/healthcheck", require("./routes/healthcheck.routes"));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
 
 app.get("/", (req, res) => {
   headers = { http_status: 200, "cache-control": "no-cache" };
@@ -26,10 +29,12 @@ app.get("/registerscore", (req, res) => {
   res.status(200).send({"status":"success"});
 });
 
-app.get("/post", (req ,res) => {
+app.post("/registerscore", (req ,res) => {
   headers={"http_status":200, "cache-control":  "no-cache"}
-  let user = req.body.user;
-  let score = req.body.score;
+  let data = JSON.parse(req.body)
+  let user = data.user;
+  let score = data.score;
+  console.log(user + "->" + score)
   users[user] = score;
   res.status(200).send({"status":"success"});
 });
@@ -47,3 +52,5 @@ app.post("/echo", (req, res) => {
 app.listen(PORT, () => {
   console.log(`STARTED LISTENING ON PORT ${PORT}`);
 });
+
+
