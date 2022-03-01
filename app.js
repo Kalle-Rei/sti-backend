1ba9605948e0e7ca96464f3b45763354a992d301
Karl Reithmeier <kalle_reithmeier@hotmail.com>
2022-03-01 17:41:36 +0100

    added /echo

--- app.js
+++ app.js
@@ -38,6 +38,11 @@ app.get("/highscores", (req ,res)=>{
   res.status(200).send(users);
 });
 
+app.post('/echo', (req, res) => {
+  console.log(JSON.stringify(req.body))
+  res.status(200).send(req.body)
+});
+
 
 
 // app.get("/hypnosismic", (req, res) => {
